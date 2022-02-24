60b547868a34321949d033dc94de30216d6e981c
David Unay Santisteban <davidunay.santisteban@cloudblue.com>
2022-02-24 20:33:17 +0100

    Feature/Added filesystem cache.

---
+++ py_cache/fs.py
@@ -0,0 +1,60 @@
+import os
+import time
+import glob
+from typing import Any, Optional
+
+import jsonpickle
+
+from py_cache.contracts import Cache
+
+
+class FileSystemCache(Cache):
+    def __init__(self, directory_path: str, ttl: int = 900):
+        self.directory_path = directory_path.strip().rstrip('/')
+        self.ttl = ttl
+
+    def _get_file_name(self, key: str) -> str:
+        return f'{self.directory_path}/cache.{key}.json'
+
+    def has(self, key: str) -> bool:
+        return self.get(key) is not None
+
+    def get(self, key: str, default: Any = None) -> Any:
+        cache_file_name = self._get_file_name(key)
+        try:
+            with open(cache_file_name, 'r') as cache:
+                content = jsonpickle.decode(cache.read())
+
+            if int(time.time() - os.path.getmtime(cache_file_name)) >= content['ttl']:
+                os.remove(self._get_file_name(key))
+                raise FileNotFoundError
+
+            return content['value']
+        except FileNotFoundError:
+            ttl = self.ttl
+            value = default() if callable(default) else default
+
+            if isinstance(value, tuple):
+                value, ttl = value
+
+        return value if value is None else self.put(key, value, ttl)
+
+    def put(self, key: str, value: Any, ttl: Optional[int] = None) -> Any:
+        with open(self._get_file_name(key), 'w') as cache:
+            cache.write(jsonpickle.encode({
+                'ttl': ttl if ttl else self.ttl,
+                'value': value,
+            }))
+
+        return value
+
+    def delete(self, key: str) -> None:
+        try:
+            print(self._get_file_name(key))
+            os.remove(self._get_file_name(key))
+        except FileNotFoundError:
+            pass
+
+    def flush(self) -> None:
+        for file in glob.glob(self._get_file_name('*')):
+            os.remove(file)
